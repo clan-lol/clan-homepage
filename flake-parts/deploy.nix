fa999d55f5766853526db3cb5839beb67309bdbb
DavHau <hsngrmpf+github@gmail.com>
2023-07-17 23:33:16 +0200

    workflows: add deployment workflow

--- flake-parts/deploy.nix
+++ flake-parts/deploy.nix
@@ -11,7 +11,16 @@
         pkgs.rsync
       ]}"
 
-      rsync -a ${self'.packages.default}/ root@clan.lol:/var/www
+      if [ -n "$SSH_HOMEPAGE_KEY" ]; then
+        sshExtraArgs="-i $SSH_HOMEPAGE_KEY"
+      else
+        sshExtraArgs=
+      fi
+
+      rsync \
+        -e "ssh -o StrictHostKeyChecking=no $sshExtraArgs" \
+        -a ${self'.packages.default}/ \
+        www@clan.lol:/var/www
     '';
   in {
     apps.deploy.program = "${deployScript}";
