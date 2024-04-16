da723063b32bfcdb7914c5819f35d839fb9eaabd
Jörg Thalheim <joerg@thalheim.io>
2024-04-16 15:05:35 +0200

    publish status update

---
+++ content/blog/2024-04-16-weekly-clan-status-update.md
@@ -0,0 +1,13 @@
++++
+title = "Weekly clan status update"
+date = 2024-04-16T14:36:16+02:00
+draft = false
+template = "blog/page.html"
+
+[taxonomies]
+authors = ["Lassulus", "Mic92"]
++++
+
+We added a new documentation hub for clan at [docs.clan.lol](https://docs.clan.lol)
+We now have weekly office hours where people are invited to hangout and ask questions.
+They are every Wednesday 15:30 UTC (17:30 CEST) at https://jitsi.lassul.us/clan.lol
--- flake-parts/website.nix
+++ flake-parts/website.nix
@@ -1,53 +1,53 @@
+{ self, ... }:
 {
-  self,
-  inputs,
-  ...
-}: {
-  perSystem = {
-    lib,
-    pkgs,
-    self',
-    inputs',
-    ...
-  }: let
-    build = baseUrl: pkgs.runCommand "website" {
-      buildInputs = [ pkgs.zola ];
-    } ''
-      mkdir -p $out
-      cp -r ${self}/* .
-      chmod -R u+w .
+  perSystem =
+    {
+      lib,
+      pkgs,
+      self',
+      inputs',
+      ...
+    }:
+    let
+      build =
+        baseUrl:
+        pkgs.runCommand "website" { buildInputs = [ pkgs.zola ]; } ''
+          mkdir -p $out
+          cp -r ${self}/* .
+          chmod -R u+w .
 
-      substituteInPlace config.toml --replace \
-        'base_url = "https://clan.lol"' \
-        'base_url = "${baseUrl}"' \
+          substituteInPlace config.toml --replace \
+            'base_url = "https://clan.lol"' \
+            'base_url = "${baseUrl}"' \
 
-      # generates a zola compatible .md from a clan-core/docs/**/*.md
-      generatePage() {
-        local sourceFile="$1"
-        local targetFile="$2"
+          # generates a zola compatible .md from a clan-core/docs/**/*.md
+          generatePage() {
+            local sourceFile="$1"
+            local targetFile="$2"
 
-        # generate title by reading first non-empty line of $file and stripping all '#' symbols
-        title=$(sed -n '/./{p;q}' "$sourceFile" | sed 's/#*//g')
-        echo "generating page from clan-core: $title"
+            # generate title by reading first non-empty line of $file and stripping all '#' symbols
+            title=$(sed -n '/./{p;q}' "$sourceFile" | sed 's/#*//g')
+            echo "generating page from clan-core: $title"
 
-        # generate header with title, template, weight to make zola happy
-        echo -e "+++\ntitle = \"$title\"\ntemplate = \"docs/page.html\"\nweight = 0\n+++" > "$targetFile"
+            # generate header with title, template, weight to make zola happy
+            echo -e "+++\ntitle = \"$title\"\ntemplate = \"docs/page.html\"\nweight = 0\n+++" > "$targetFile"
 
-        # append everything from the file but remove header line starting with '#' and all preceding non-empty lines
-        tail -n +2 "$sourceFile" >> "$targetFile"
-      }
+            # append everything from the file but remove header line starting with '#' and all preceding non-empty lines
+            tail -n +2 "$sourceFile" >> "$targetFile"
+          }
 
-      zola build
-      cp -r public/* public/.* $out
-    '';
-  in {
-    packages.default = self'.packages.website;
-    packages.website = build "https://clan.lol";
-    packages.website-localhost = build "http://localhost:1111";
-    packages.serve = pkgs.writeShellScriptBin "serve-local" ''
-      echo "serving: ${self'.packages.website-localhost}"
-      ${pkgs.python3}/bin/python -m http.server 1111 \
-        -d ${self'.packages.website-localhost}
-    '';
-  };
+          zola build
+          cp -r public/* public/.* $out
+        '';
+    in
+    {
+      packages.default = self'.packages.website;
+      packages.website = build "https://clan.lol";
+      packages.website-localhost = build "http://localhost:1111";
+      packages.serve = pkgs.writeShellScriptBin "serve-local" ''
+        echo "serving: ${self'.packages.website-localhost}"
+        ${pkgs.python3}/bin/python -m http.server 1111 \
+          -d ${self'.packages.website-localhost}
+      '';
+    };
 }
