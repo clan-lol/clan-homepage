59c43cb228623c5632afb3b2b88524048096e50a
Qubasa <consulting@qube.email>
2024-05-23 15:48:33 +0200

    Add js animation

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -1,6 +1,38 @@
-/*
-	Directive by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+function hacker_effect(target) {
+	let iteration = 0;
+	clearInterval(interval);
+
+	interval = setInterval(() => {
+		target.innerText = target.innerText
+			.split("")
+			.map((letter, index) => {
+				if (index < iteration) {
+					return target.dataset.value[index];
+				}
+
+				return letters[Math.floor(Math.random() * 26)]
+			})
+			.join("");
+
+		if (iteration >= target.dataset.value.length) {
+			clearInterval(interval);
+		}
+
+		iteration += 1 / 3;
+	}, 30);
+}
+
+document.querySelector("span").onmouseover = event => hacker_effect(event.target);
+	window.onload = function () {
+		var delay = 250;
+		setTimeout(function () {
+			let target = document.querySelector("span");
+			hacker_effect(target);
+		}, delay);
+	};
